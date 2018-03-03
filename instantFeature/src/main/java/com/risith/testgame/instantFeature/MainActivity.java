dbeda1cdf24906c4e540b3194d9c59968131d302
Risith Perera <dgassnerdev@gmail.com>
2018-03-03 15:32:40 +0530

    Initial commit

---
+++ instantFeature/src/main/java/com/risith/testgame/instantFeature/MainActivity.java
@@ -0,0 +1,18 @@
+package com.risith.testgame.instantFeature;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GamePanel(this));
+        System.out.println("Hello");
+    }
+}
